5b2a5e2da514f45e8302fc4997d4e8ac872cc8b2
zlt-dllg <1185701866@qq.com>
2026-02-22 02:15:42 +0800

    2.22 学习git相关操作，运用sourcetree将前期合作完成作品一次性上传至github云端仓库。

---
+++ python_miniblog_now（合作设计版本）/python_miniblog/PythonProject/models.py
@@ -0,0 +1,42 @@
+from extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    signature = db.Column(db.String(128), default='未设置个性签名')  # 新增
+    # 关系：1个用户可以有多个博客文章
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+   # created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    category = db.Column(db.String(20), default='技术求助')  # 帖子分类
+    created_at = db.Column(db.DateTime, default=datetime.now)  # 创建时间
+
+    # 外键关联用户
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
